441830290884a95f4f8ff7188e26da9ad3994fa9
nahyunkwon <skgus2624@gmail.com>
2023-08-23 12:08:29 -0500

    add detection skleton error fix

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/detection', method=['GET', 'POST'])
+def detection():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('detection.html')
 
 @app.route('/about')
 def about():
