9596c06a1286149cc31f4ef8939601688c74317c
nahyunkwon <skgus2624@gmail.com>
2023-08-27 04:14:21 -0500

    add minor interaction for hoverover

--- api/static/assets/js/suggestion.js
+++ api/static/assets/js/suggestion.js
@@ -1,6 +1,10 @@
 function update_suggestion(object){
+    suggestion_title = document.getElementById('suggestion-title');
     suggestion = document.getElementById('suggestion');
 
-    suggestion.innerHTML = object;
+    suggestion_title.innerHTML = "<h3>" + object + "</h3>"
+
+    suggestion.innerHTML = "<h4>" + object + "</h4>"
+        + "Test";
 
 }
