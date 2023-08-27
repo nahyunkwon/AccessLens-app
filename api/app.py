1421f6ba39c5b818940360325b14af88505064be
Nahyun Kwon <skgus2624@gmail.com>
2023-08-27 02:01:09 -0500

    demo page

--- api/app.py
+++ api/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/detection')
-def detection():
+@app.route('/main')
+def main():
     # if request.method == 'POST':
     #     # do stuff when the form is submitted
 
@@ -16,7 +16,19 @@ def detection():
     #     return redirect(url_for('index'))
 
     # show the form, it wasn't submitted
-    return render_template('detection.html')
+    return render_template('main.html')
+
+@app.route('/demo')
+def demo():
+    # if request.method == 'POST':
+    #     # do stuff when the form is submitted
+
+    #     # redirect to end the POST handling
+    #     # the redirect can be to the same route or somewhere else
+    #     return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('demo.html')
 
 @app.route('/about')
 def about():
