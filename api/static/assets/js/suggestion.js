3773444b09519835107c89a2448e8f031904f4ab
nahyunkwon <skgus2624@gmail.com>
2023-08-27 06:04:35 -0500

    main page modified

--- api/static/assets/js/suggestion.js
+++ api/static/assets/js/suggestion.js
@@ -4,7 +4,23 @@ function update_suggestion(object){
 
     suggestion_title.innerHTML = "<h3>" + object + "</h3>"
 
-    suggestion.innerHTML = "<h4>" + object + "</h4>"
-        + "Test";
+    // suggestion.innerHTML = "<h4>" + object + "</h4>";
+
+    // open ul element
+    // suggestion.innerHTML = ""
+    suggestion.innerHTML = suggestion.innerHTML + "<ul>\n";
+
+
+    // add list component
+    design_title = "title1"
+    design_img = "static/assets/images-design/design.jpg"
+    design_full_link = "https://www.thingiverse.com/thing:2271048"
+    suggestion.innerHTML = suggestion.innerHTML + "<li>" + design_title + "<img class='design' src=" + design_img + "><li>\n";
+
+    // close ul element
+    suggestion.innerHTML = suggestion.innerHTML + "</ul>";
+       
+
 
 }
+
